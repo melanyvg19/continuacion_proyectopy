c0ca9bc34a4fe3fc49eb91a5dbf4da49e855682a
melanyvalle <vallemelanie717@gmail.com>
2024-05-14 20:47:39 -0500

    nt13 feat se agrega siembra de arboles

---
+++ data/generators/siembraArboles.py
@@ -0,0 +1,14 @@
+import random
+
+def generarSiembraArboles():
+    listaDatos=[]
+    for i in range(1000):
+        corregimiento=random.choice(['Altavista','San Antonio de Prado', 'San Cristobal', 'Santa Elena', 'San Sebastian de Palmitas'])
+        nombre=random.choice(['Peppa Pig', 'Barney', 'Sportacous', 'Gilberto Jimenez', 'Rodolfo Hernandez', 'Gustavo Petro', 'Alvaro Uribe', 'Donald Trump', 'Cesar Gaviria', 'J Balvin', 'Nicolas Mora', 'Beatriz Pinzon', 'German Quintero'])
+        id=random.randint(0,1000000)
+        hectareasSembradasAño=random.randit(0,50000)
+        especieSembrada=random.choice(['Samanes', 'cedros', 'chochos', 'ébanos', 'acacias amarillas y rojas', 'guayabillos', 'caobas'])
+        siembraArboles=[corregimiento,nombre,id,hectareasSembradasAño,especieSembrada]
+    return listaDatos
+
+generarSiembraArboles()
