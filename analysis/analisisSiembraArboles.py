b2f814d7307b263477c795dd9219dd82b139ec98
melanyvalle <vallemelanie717@gmail.com>
2024-06-11 16:19:11 -0500

    se crea promedio siembra arboles

--- analysis/analisisSiembraArboles.py
+++ analysis/analisisSiembraArboles.py
@@ -2,13 +2,50 @@ import pandas as pd
 from data.generators.siembraArboles import generarSiembraArboles
 from helpers.generarTabla import crearTablaHTML
 
-def contruirDTSiembraArboles():
+def construirDTSiembraArboles():
 
     datosSiembraArboles=generarSiembraArboles()
 
-    siembraArbolesDF=pd.DataFrame(datosSiembraArboles, columns=['corregimineto', 'nombre', 'id', 'hectareascanio', 'especie'])
-
-    print(siembraArbolesDF)
+    siembraArbolesDF=pd.DataFrame(datosSiembraArboles, columns=['corregimiento', 'nombre', 'id', 'hectareasSembradasAnio', 'especie'])
     crearTablaHTML(siembraArbolesDF, "TablaSiembraArboles")
 
-contruirDTSiembraArboles()
+    siembraArbolesDF.replace('sin', pd.NA, inplace=True)
+    siembraArbolesDF.replace('-', pd.NA, inplace=True)
+    siembraArbolesDF.replace('no', pd.NA, inplace=True)
+
+    nivelBajo =siembraArbolesDF.query("(hectareasSembradasAnio <= 100)")
+    nivelMedio = siembraArbolesDF.query("(hectareasSembradasAnio > 100) and (hectareasSembradasAnio <= 1000)")
+    nivelAlto = siembraArbolesDF.query("(hectareasSembradasAnio > 1000)")
+
+    deficit = nivelBajo.groupby('corregimiento')['hectareasSembradasAnio'].count()    
+    moderado = nivelMedio.groupby('corregimiento')['hectareasSembradasAnio'].count()
+    superavit = nivelAlto.groupby('corregimiento')['hectareasSembradasAnio'].count()   
+
+    print('Niveles bajos por corregimiento:', deficit)
+    print('Niveles medios por corregimiento:', moderado)
+    print('Niveles altos por corregimiento:', superavit)
+
+    sumaDeficit = nivelBajo.groupby('corregimiento')['hectareasSembradasAnio'].sum()
+    sumaModerado = nivelMedio.groupby('corregimiento')['hectareasSembradasAnio'].sum()
+    sumaSuperavit = nivelAlto.groupby('corregimiento')['hectareasSembradasAnio'].sum()
+
+    promedioDeficit = sumaDeficit / deficit
+    promedioDeficitDF = promedioDeficit.reset_index().rename(columns={'hectareasSembradasAnio':'Promedio en Deficit'})
+    crearTablaHTML(promedioDeficitDF, "promedioDeficit")    
+
+    promedioModerado = sumaModerado / moderado
+    promedioModeradoDF = promedioModerado.reset_index().rename(columns={'hectareasSembradasAnio':'Promedio Moderado'})
+    crearTablaHTML(promedioModeradoDF, "promedioModerado")
+
+    promedioSuperavit = sumaSuperavit / superavit    
+    promedioSuperavitDF = promedioSuperavit.reset_index().rename(columns={'hectareasSembradasAnio':'Promedio Superavit'})
+    crearTablaHTML(promedioSuperavitDF, "promedioSuperavit")
+
+    print('Promedio Siembra bajo por corregimiento:')
+    print(promedioDeficit)
+    print('\nPromedio Siembra moderado por corregimiento:')
+    print(promedioModerado)
+    print('\nPromedio Siembra superavit por corregimiento:')
+    print(promedioSuperavit)  
+
+construirDTSiembraArboles()
--- data/generators/siembraArboles.py
+++ data/generators/siembraArboles.py
@@ -6,11 +6,10 @@ def generarSiembraArboles():
         corregimiento=random.choice(['Altavista','San Antonio de Prado', 'San Cristobal', 'Santa Elena', 'San Sebastian de Palmitas'])
         nombre=random.choice(['Peppa Pig', 'Barney', 'Sportacous', 'Gilberto Jimenez', 'Rodolfo Hernandez', 'Gustavo Petro', 'Alvaro Uribe', 'Donald Trump', 'Cesar Gaviria', 'J Balvin', 'Nicolas Mora', 'Beatriz Pinzon', 'German Quintero'])
         id=random.randint(0,1000000)
-        hectareasSembradasAño=random.randint(0,50000)
+        hectareasSembradasAnio=random.randint(0,50000)
         especieSembrada=random.choice(['Samanes', 'cedros', 'chochos', 'ébanos', 'acacias amarillas y rojas', 'guayabillos', 'caobas'])
-        siembraArboles=[corregimiento,nombre,id,hectareasSembradasAño,especieSembrada]
+        siembraArboles=[corregimiento,nombre,id,hectareasSembradasAnio,especieSembrada]
         
         listaDatos.append(siembraArboles)
 
     return listaDatos
-
